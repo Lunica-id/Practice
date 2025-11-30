6ce56ae16a86df0f5f79d1f8ba4c552d49f4ca35
Lunica-id <id.lunica.work@gmail.com>
2025-11-29 21:43:47 -0800

    update file

--- counter/script.js
+++ counter/script.js
@@ -1,36 +1,55 @@
 const plus = document.getElementById('plus');
 const minus = document.getElementById('minus');
-const strNum = document.getElementById('number');
+const numberElem = document.getElementById('number');
 const reset = document.getElementById('reset');
-let number = parseInt(strNum.innerText, 10);
+let number = parseInt(numberElem.innerText, 10);
+let intervalId = null; 
 
-plus.addEventListener('click', () => {
-    number += 1;
-    strNum.innerText = number;
-    numberColour(number);
-})
-
-minus.addEventListener('click', () => {
-    number -= 1;
-    strNum.innerText = number;
-    numberColour(number);
-})
-
+plus.addEventListener('click', () => update(1));
+minus.addEventListener('click', () => update(-1));
 reset.addEventListener('click', () => {
     number = 0;
-    strNum.innerText = number;
-    numberColour(number);
+    numberElem.innerText = number;
+    updateColour(number);
 })
 
-function numberColour(num) {
+function startHoldChange(step) {
+    if (intervalId) return;
+
+    intervalId = setInterval(() => {
+        update(step);
+    }, 100);
+}
+
+function stopHoldChange() {
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+plus.addEventListener('mousedown', ()=> startHoldChange(1));
+plus.addEventListener('mouseup', stopHoldChange);
+plus.addEventListener('mouseleave', stopHoldChange);
+
+minus.addEventListener('mousedown', ()=> startHoldChange(-1));
+minus.addEventListener('mouseup', stopHoldChange);
+minus.addEventListener('mouseleave', stopHoldChange);
+
+function update(step) {
+    number += step;
+    numberElem.innerText = number;
+    updateColour(number);
+}
+
+
+function updateColour(num) {
     if (num === 0) {
-        strNum.classList.remove('plus');
-        strNum.classList.remove('minus');
+        numberElem.classList.remove('plus');
+        numberElem.classList.remove('minus');
     } else if (num < 0) {
-        strNum.classList.remove('plus');
-        strNum.classList.add('minus');
+        numberElem.classList.remove('plus');
+        numberElem.classList.add('minus');
     } else {
-        strNum.classList.remove('minus');
-        strNum.classList.add('plus');
+        numberElem.classList.remove('minus');
+        numberElem.classList.add('plus');
     }
 }
